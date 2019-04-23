d48828c51455660ebb0e8ea12c934c0c544bb1e4
Kieran McManus <kmcmanus41092@gmail.com>
2019-04-23 11:53:31 -0400

    First commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// Responsive Nav
+$('#hamburger').on('click', function() {
+    $('.nav2 ul').toggle();
+    $('.nav2').toggleClass('block');
+});
+
+// Nav toggle on scroll
+$(window).on('scroll', function () {
+	// Step 1: Google $(window).scrollTop();
+	// Find out how far they've scrolled
+	var distanceScrolled = $(window).scrollTop();
+
+	console.log('The distance scrolled is: ' + distanceScrolled);
+
+  var w = window.innerWidth;
+
+  if (distanceScrolled < 30 && w > 870) {
+    $('.main_nav').hide();
+  } else if (distanceScrolled > 30 && w > 870) {
+    $('.main_nav').fadeIn(350);
+  }
+});
+
+// Image carousel
+var currentImageNumber = 0;
+var numberOfImages = $('img').length - 1;
+console.log("There are " + numberOfImages + "images");
+$('#next').on('click', function () {
+   if (currentImageNumber < numberOfImages) {
+     currentImageNumber += 1;
+   } else {
+     currentImageNumber = 0;
+   }
+   $('img').fadeOut(0);
+   $('img').eq(currentImageNumber).fadeIn(0);
+});
+
+$('#previous').on('click', function () {
+   if (currentImageNumber > 0) {
+     currentImageNumber -= 1;
+   } else {
+     currentImageNumber = numberOfImages;
+   }
+   $('img').fadeOut(0);
+   $('img').eq(currentImageNumber).fadeIn(0);
+});
